d144f8aaf132595bbb7175b84fa563bb94a84cf0
Vinicius Metri <vinicius.metri.dev@gmail.com>
2024-09-09 11:48:22 -0300

    calculadora

---
+++ calculadora.py
@@ -0,0 +1,7 @@
+numero1 = float(input("Digite um numero: "))
+numero2 = float(input("Digite outro numero: "))
+
+print(f"a soma é = {numero1 + numero2}")
+print(f"a subtração é = {numero1 - numero2}")
+print(f"a multiplicação é = {numero1 * numero2}")
+print(f"a divisão é = {numero1 / numero2}")
